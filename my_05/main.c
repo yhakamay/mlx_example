5d8442b6ffa99ae03303add93a5defc880072d03
yhakamay <yhakamay@student.42tokyo.jp>
2021-02-24 20:58:28 +0900

    add: #define WINDOW_NAME
    the name is 'test' so far...

--- my_05/main.c
+++ my_05/main.c
@@ -6,12 +6,13 @@
 /*   By: yhakamay <yhakamay@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/24 20:48:06 by yhakamay          #+#    #+#             */
-/*   Updated: 2021/02/24 20:48:20 by yhakamay         ###   ########.fr       */
+/*   Updated: 2021/02/24 20:54:47 by yhakamay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../mlx/mlx.h"
 
+#define WINDOW_NAME "test"
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 
@@ -35,5 +36,12 @@ typedef struct	s_mlx
 
 int	main(void)
 {
-	
+	void	*mlx;
+	void	*win;
+	t_img	img;
+	int		count_h;
+	int		count_w;
+
+	mlx = mlx_init();
+	win = mlx_new_window(mlx, WINDOW_HEIGHT, WINDOW_WIDTH, WINDOW_NAME);
 }
